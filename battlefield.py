3125405f7d5c83cfa098b0e6054825313fca4a78
John Parks <jcparks1984@gmail.com>
2022-02-03 15:07:27 -0500

    added battlefield.py and weapon.py

---
+++ battlefield.py
@@ -0,0 +1,29 @@
+from herd import Herd
+class Battlefield:
+    def __init__(self):
+        self.fleet = ''
+        self.herd = ''
+
+    def run_game(self):
+        pass
+
+    def display_welome(self):
+        pass
+
+    def fleet_vs_herd_battle(self):
+        pass
+
+    def dino_turn(self):
+        pass
+
+    def robo_turn(Self):
+        pass
+    
+    def show_dino_opponent_options(self):
+        pass
+
+    def show_robo_opponent_options(self):
+        pass
+    
+    def display_winners(self):
+        pass
---
+++ weapon.py
@@ -0,0 +1,14 @@
+class weapon:
+    def __init__(self):
+        self.weapon_name = ''
+        self.weapon_attack_power = ''
+
+    def set_weapon_name(self):
+        self.weapon_name = ''
+        print(self.weapon_name)
+        pass
+
+    def set_weapon_attack_power(self):
+        self.weapon_attack_power = ''
+        print(self.weapon_attack_power)
+        pass
