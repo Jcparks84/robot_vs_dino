2015e6b06051125a461f23c1f14b8ff83d07eae0
John Parks <jcparks1984@gmail.com>
2022-02-03 14:44:49 -0500

    created dinosaur.py and herd.py

---
+++ dinosaur.py
@@ -0,0 +1,20 @@
+class Dinosaur:
+    def __init__(self):
+        self.dino_name = ''
+        self.dino_attack = ''
+        self.dino_health = ''
+    
+    def set_dino_name(self):
+        self.dino_name = ''
+        print(self.dino_name)
+        pass
+
+    def set_dino_attack(Self):
+        Self.dino_attack = ''
+        print(Self.dino_attack)
+        pass
+
+    def set_dino_health(self):
+        self.dino_health = ''
+        print(self.dino_health)
+        pass
---
+++ herd.py
@@ -0,0 +1,10 @@
+from dinosaur import Dinosaur
+
+
+class Herd:
+    def __init__(self):
+        self.dino_list = ['']
+
+    def set_dino_list(self):
+        self.dino_list = ['']
+        print(self.dino_list)
